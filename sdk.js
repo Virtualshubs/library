985f00fa31c5d31d75549391a50b75bc85b15abe
Virtualshubs <mmateriaa@gmail.com>
2026-03-13 12:55:20 +0100

    Update sdk.js

--- sdk.js
+++ sdk.js
@@ -1,4 +1,4 @@
-// sdk.js
+
 (function(global){
   function init(options){
     const container = document.getElementById(options.containerId);
@@ -6,21 +6,18 @@
 
     const base64Scene = container.dataset.scene;
 
-    // Construir la URL que el visor original va a interpretar
-    const visorURL = `https://viewer.3dtwins.tech/embeb.html?t=${encodeURIComponent(base64Scene)}`;
+    const visorURL = `https://viewer.3dtwins.tech/embeb.html?t=${base64Scene}`;
 
-    // Crear iframe
+    
     const iframe = document.createElement('iframe');
     iframe.src = visorURL;
     iframe.width = '100%';
     iframe.height = '100%';
     iframe.style.border = 'none';
 
-    // Limpiar contenedor y agregar iframe
     container.innerHTML = '';
     container.appendChild(iframe);
   }
 
-  // Exponer función global para que el usuario pueda iniciar la escena
   global.VH = { init };
 })(window);
