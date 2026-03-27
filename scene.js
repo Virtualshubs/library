85ad80bde3c05c31ba980e00262f9135c2b58a94
Virtualshubs <mmateriaa@gmail.com>
2026-03-27 22:23:08 +0100

    Update scene.js

--- scene.js
+++ scene.js
@@ -1,49 +1,17 @@
-buyButton.onclick = () => {
+ (function(){
+      const urlParams = new URLSearchParams(window.location.search);
+      let base64Scene = '';
+      if(urlParams.has('scene')) {
+        base64Scene = urlParams.get('scene');
+      }
 
-  if (!sceneParam) {
-    alert("No hay configuración disponible");
-    return;
-  }
-
-  let sceneBuyURL = '';
-
-  try {
-    const decodedScene = atob(decodeURIComponent(sceneParam));
-    const sceneURL = new URL(decodedScene);
-    const buyParam = sceneURL.searchParams.get("buyURL");
-
-    if (buyParam) {
-      sceneBuyURL = decodeURIComponent(buyParam);
-    }
-  } catch (e) {
-    console.warn("Error leyendo buyURL:", e);
-  }
-
- 
-  if (sceneBuyURL && sceneBuyURL.startsWith("https://")) {
-
-    const baseURL = sceneBuyURL.endsWith('/')
-      ? sceneBuyURL.slice(0, -1)
-      : sceneBuyURL;
-
-    const finalBuyURL = `${baseURL}/products/personalizado?scene=${encodeURIComponent(sceneParam)}`;
-
- 
-    window.location.href = finalBuyURL;
-    return;
-  }
-
- 
-  const newURL = window.location.href.replace(
-    "product.3dtwins.tech",
-    "scene.3dtwins.tech"
-  );
-
-  window.history.replaceState({}, '', newURL);
-
-
-  VH.init({
-    containerId: 'vh-container',
-    scene: sceneParam
-  });
-};
+      if(!base64Scene) {
+        console.error("No se ha proporcionado escena en la URL");
+        return;
+      }
+      VH.init({
+        containerId: 'vh-container',
+        scene: base64Scene
+      });
+      history.replaceState({}, '', `?scene=${base64Scene}`);
+    })();
